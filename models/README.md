f54be67fe1ae8865d9c6cd70500d34e59bf654cb
SFRL <sebastian.lobbers@googlemail.com>
2025-08-20 12:09:12 +0000

    feat: enhance summarization functionality and add new YAML prompts for CLPsych 2025 tasks

--- models/README.md
+++ models/README.md
@@ -6,6 +6,8 @@ This directory contains models for various tasks, e.g. summarisation
 
 The directory <a href="example_data">example_data</a> contains small examples for testing and demonstration purposes.
 
+The directory <a href="prompts">prompts</a> contains YAML files with prompts for various tasks.
+
 ## summary_generation_with_autoclass.py
 
 This script implements summary generation with <a href="https://huggingface.co/docs/transformers/en/model_doc/auto#transformers.AutoModelForCausalLM">AutoModelForCausalLM</a> from the AutoClass module in the Hugging Face Transformers library. The script allows to load Huggingface models and perform summary generation tasks. It also includes an implementation of the summary generation from the paper [Temporal reasoning for timeline summarisation in social media](https://aclanthology.org/2025.acl-long.1362/) (Song et al., ACL 2025). An example of how to use the script is provided in <a href="generation_examples.ipynb">generation_examples.ipynb</a>.
--- models/summary_generation_with_autoclass.py
+++ models/summary_generation_with_autoclass.py
@@ -67,30 +67,38 @@ class LLMGenerator:
         ]
 
     def generate(self, prompt: str, text: str, max_tokens: int = 5000, temperature: float = 0.0):
+        sampling = temperature > 0
+        gen_kwargs = {
+            "max_new_tokens": max_tokens,
+            "eos_token_id": self.terminators,
+            "do_sample": sampling,
+        }
+
+        if sampling:
+            gen_kwargs.update({
+                "temperature": temperature,
+                "top_p": 0.9,             
+            })
         with torch.no_grad():
             outputs = self.pipe(
                         self.create_prompt_message(prompt, text),
-                        max_new_tokens=max_tokens,
-                        eos_token_id=self.terminators,
-                        do_sample=False if temperature == 0 else True,
-                        temperature=temperature,
-                        top_p=0.9,
+                        **gen_kwargs
                     )
             return outputs[0]["generated_text"][-1]["content"]
     
-    def run_summary(self, prompt: List[str], text: List[str], max_tokens: List[int], temperature: float = 0.0):
+    def run_summary(self, prompt: List[str], text: List[str], max_tokens: List[int], temperatures: List[float]):
         """
            Generate summaries in n number of rounds. The list of prompts is interpreted as how many rounds of summaries to generate.
            For example, it could be that the content of posts will be summarised first and then these summaries will be summarised
            again to create a timeline summary.
 
-           prompt and max_tokens should be lists of the same length, where each element corresponds to a round of summarisation.
+           prompt, max_tokens and temperatures should be lists of the same length, where each element corresponds to a round of summarisation.
            list is passed as a list in case the original material is presented as multiple documents. 
            However, the output of the generation will be concatenated into a single string. So from the 2nd round onwards,
            the summarisation will be applied to one string rather than a list of strings.
         """
 
-        for p, max in zip(prompt, max_tokens):
+        for p, max, temperature in zip(prompt, max_tokens, temperatures):
             summary = []
             for t in text:
                 current_summary = self.generate(
